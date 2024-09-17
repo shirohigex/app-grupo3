7e3e973f940d5fdd3e0d43e20dbba01f87246f18
Academico <Academico@T08KETIPC07C203>
2024-09-17 13:55:59 -0500

    feat: agregado conexion

---
+++ app-grupo3/Data/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace app_grupo3.Data
+{
+    public class Conexion
+    {
+        private MySqlConnection cnx = new MySqlConnection("");
+
+        public MySqlConnection Conectar
+        {
+            get { return cnx; }
+        }
+        public void Desconectar()
+        {
+            cnx.Close();
+        }
+    }
+}
